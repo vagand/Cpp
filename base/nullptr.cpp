eea1df135d2bbafa69840a2e86246a54102766e5
기동욱/선임연구원/ESS제어개발팀(donguk.ki@lge.com) <donguk.ki@lge.com>
2023-01-09 17:19:25 +0900

    base

---
+++ base/nullptr.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    int *a = 0;
+    int b = 0;
+    int *c = nullptr;
+
+    return 0;
+}
---
+++ base/uniform_initialize.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+struct Point {
+    int x;
+    int y;
+};
+
+int main()
+{
+    int n {1};
+	Point p {1,2};
+	int x[] {1,2,3};
+
+    return 0;
+}
